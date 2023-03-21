80a838b735f40a6716e34cb71a0789584668b03d
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2023-03-21 23:12:02 +0100

    v2

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,8 +5,8 @@
 #=================================================
 
 # dependencies used by the app
-pkg_dependencies="ffmpeg postgresql postgresql-contrib openssl g++ mailutils apt-transport-https"
-app_dependencies="prosody"
+#REMOVEME? pkg_dependencies="ffmpeg postgresql postgresql-contrib openssl g++ mailutils apt-transport-https"
+#REMOVEME? app_dependencies="prosody"
 
 nodejs_version=16
 

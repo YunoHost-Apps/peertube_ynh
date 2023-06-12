b8aaa6bdadec005a3eac231b72f6218c4e1662db
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2023-06-12 12:37:56 +0200

    cleaning

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,7 +4,7 @@
 # COMMON VARIABLES
 #=================================================
 
-nodejs_version=16
+nodejs_version=18
 
 #=================================================
 # PERSONAL HELPERS

33867c2eb54d10b68492ad67863472d16c8248e0
yalh76 <yalh@yahoo.com>
2023-08-04 02:20:17 +0200

    Fix #375

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -6,7 +6,6 @@
 
 # dependencies used by the app
 pkg_dependencies="ffmpeg postgresql postgresql-contrib openssl g++ mailutils apt-transport-https"
-app_dependencies="prosody"
 
 nodejs_version=16
 

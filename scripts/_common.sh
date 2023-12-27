63433a064671fbd657faba9b890aa72af31f3e79
Alexandre Aubin <alex.aubin@mailoo.org>
2023-12-27 16:13:33 +0100

    Attempt to fix the db suffix thingy...

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,6 +5,8 @@
 #=================================================
 
 nodejs_version=20
+# Obtain the (empty string), __2, __3, cf the DB suffix in production.yaml...
+db_suffix="$(echo $app | sed 's/peertube//g')"
 
 #=================================================
 # PERSONAL HELPERS
